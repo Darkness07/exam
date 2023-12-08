789e699baed0da57445974f102e72abe53764bca
Нурислам <vnurlanov@gmail.com>
2023-12-08 20:07:09 +0600

    проеет, начало

---
+++ src/Routes/MainRouter.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import Home from '../pages/Home/Home'
+import Contacts from '../pages/Contacts/Contacts'
+import About from '../pages/About/About'
+
+export default function MainRouter() {
+    const nuEtoDa = [
+        { link: "/", element: <Home />, id: 1 },
+        { link: "/Contacts", element: <Contacts />, id: 2 },
+        { link: "/About", element: <About />, id: 3 },
+    ]
+    return (
+        <Routes>
+            {
+                nuEtoDa.map((item) => (
+                    <Route path={item.link} element={item.element} key={item.id}></Route>
+                ))
+            }
+        </Routes>
+    )
+}
---
+++ src/pages/ItemCards/ItemCards.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function ItemCards() {
+  return (
+    <div className='ItemCards'></div>
+  )
+}
