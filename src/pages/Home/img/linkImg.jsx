7a42e27e5ff92e3523d0d1674d6b0650bb960cc3
Нурислам <vnurlanov@gmail.com>
2023-12-12 15:53:45 +0600

    без адаптива

---
+++ src/pages/Home/img/linkImg.jsx
@@ -0,0 +1,31 @@
+import img1 from './marka/BMW.png'
+import img2 from './marka/geely.png'
+import img3 from './marka/Honda.png'
+import img4 from './marka/KIA.png'
+import img5 from './class/1.png'
+import img6 from './class/2.png'
+import img7 from './class/3.png'
+import img8 from './class/4.png'
+
+import img9 from './bestSelling/image_1.png'
+import img10 from './bestSelling/image_2.png'
+import img11 from './bestSelling/image_3.png'
+import img12 from './bestSelling/image_4.png'
+
+const linkImg={
+    bmw:img1,
+    geely:img2,
+    honda:img3,
+    kia:img4,
+
+    class_1:img5,
+    class_2:img6,
+    class_3:img7,
+    class_4:img8,
+
+    bestSelling_1:img9,
+    bestSelling_2:img10,
+    bestSelling_3:img11,
+    bestSelling_4:img12,
+}
+export default linkImg
